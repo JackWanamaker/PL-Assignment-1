ad66c9b6d95191954aba1b9617458381dd5510a9
Ben Summer <jackwanamaker49@gmail.com>
2024-09-12 15:48:37 -0400

    Fixed the tail recursion implementation

--- Assignment 1.rkt
+++ Assignment 1.rkt
@@ -5,10 +5,22 @@
   (define (multiply-helper s n [accs ""])
     (if (equal? n 0)
         accs
-        (string-append s (multiply-helper s (- n 1) accs))
+        (multiply-helper s (- n 1) (string-append accs s))
         )
     )
   (multiply-helper s n)
 )
+(string-multiply "Hello" 3)
+;;Question 1: LetRec example :-)
+(define (string-multiply2 s n)
+  (letrec ([helper (lambda (s n [accs ""])
+                     (if (equal? n 0)
+                         accs
+                         (string-append s (helper s (- n 1) accs))
+                         )
+                     )
+                   ])
+    (helper s n)
+   )
+  )
 
-;;
