a5c925eccba758c13a4c925b58e2b02db77075a0
Ben Summer <jackwanamaker49@gmail.com>
2024-09-12 15:15:28 -0400

    Solved question 1!

--- Assignment 1.rkt
+++ Assignment 1.rkt
@@ -1,2 +1,14 @@
 #lang racket
-//Test file
+
+;;Question 1: Tail Recursion W/Helper Function
+(define (string-multiply s n)
+  (define (multiply-helper s n [accs ""])
+    (if (equal? n 0)
+        accs
+        (string-append s (multiply-helper s (- n 1) accs))
+        )
+    )
+  (multiply-helper s n)
+)
+
+;;
